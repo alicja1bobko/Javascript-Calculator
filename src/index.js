fd931acac55e8df760079152b63aa0bf8c205f7b
AlicjaBobko <alicja_bobko@o2.pl>
2022-08-04 16:47:25 +0200

    styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style.css";
+// import "./style.css";
+import "../src/scss/styles.js";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
